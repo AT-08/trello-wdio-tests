3f51ee44c12e0833860c8c14f4ba82fd65e8b124
geremygustavo <abelmallcu123@gmail.com>
2018-12-14 14:04:07 -0400

    add doc

--- e2e/pages/teamSettings.po.js
+++ e2e/pages/teamSettings.po.js
@@ -1,9 +1,18 @@
 'use strict';
 
+/**
+ * This is the PO of a create a team setting.
+ */
 class teamSettings {
+  /**
+   * This method change the visibility of team.
+   */
   changeVisibility() {
   }
 
+  /**
+   * This method delete the team.
+   */
   deleteTeam() {
   }
 }
