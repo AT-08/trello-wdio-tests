ddef25a2a920002920875db06e331d3dabfa77c1
geremygustavo <abelmallcu123@gmail.com>
2018-12-14 16:07:24 -0400

    new fix

--- e2e/pages/sideBar.po.js
+++ e2e/pages/sideBar.po.js
@@ -4,7 +4,19 @@ const commonActions = require('../core/ui/commonActions');
 
 class sideBar {
 
+  constructor() {
+    this.selectors = {
+      'createTeamButton': 'button[class="tab__tabButton__37WIj tab__quiet__ed4jD"]',
+      'selectedBoardButton': 'a[class="tab__tab__15G2j"]',
+      'selectedHomeButton': 'a[class="tab__tabLink__3C9rw"]',
+
+
+    };
+  }
   createTeam(data) {
+
+
+
     return new teamForm();
   }
 
