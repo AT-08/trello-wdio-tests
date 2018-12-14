d93b0f894753fec765e502f871eda39cd74774ff
Rodrigo Menacho <rmenacho21@gmail.com>
2018-12-14 16:35:38 -0400

    adding methods

---
+++ e2e/pages/list.po.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const commonActions = require('../core/ui/commonActions');
+
+const card = require('../card');
+
+/**
+ * PageObject for a list in the dashboard.
+ */
+class list {
+  /**
+   * Constructor to set the locators.
+   */
+  constructor() {
+    this.cardTitleTextAreaInput = '.list-card-composer-textarea.js-card-title';
+    this.addCardButton = '.js-add-card';
+
+
+  }
+
+  /**
+   * This method create a card in a list.
+   */
+  createCard(cardName) {
+    commonActions.setValue(this.cardTitleTextAreaInput, cardName);
+    commonActions.click(this.addCardButton);
+  }
+
+  /**
+   * This method select a specific card.
+   *
+   * @param title
+   */
+  selectCard(title) {
+    this.cardBox = `.list-card-details.js-card-details ${title}`;
+    commonActions.click(this.cardBox);
+    return new card();
+  }
+}
+
+module.exports = list;
