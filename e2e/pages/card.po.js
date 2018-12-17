9dc36582c9ad10dd2baf008d748159dc739ec9b3
wilson lopez <wiltrouble@at08-linux-gui-automation.novalocal>
2018-12-17 17:27:56 -0400

    po Card

---
+++ e2e/pages/card.po.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const commonActions = require('../core/ui/commonActions');
+
+/**
+ * This is the PO of the card page
+ */
+class card {
+
+  constructor() {
+
+    this.cardName = 'js-card-name';
+
+    this.addChecklist = 'js-add-checklist-menu';
+
+    this.checklistTitle = 'js-checklist-title';
+
+    this.btnAddChecklist = 'js-add-checklist';
+
+    this.newChecklistItem = 'js-new-checklist-item-input';
+
+    this.checklistNewItemText = 'checklist-new-item-text';
+
+    this.btnConfirmDeleteChecklist = 'js-confirm';
+
+  }
+
+  /**
+   * This method create a new list.
+   */
+  addCheckList() {
+    commonActions.click(this.cardName);
+    commonActions.click(this.addChecklist);
+    commonActions.setValue(this.checklistTitle, 'nameChecklist');
+    commonActions.click(this.btnAddChecklist);
+
+  }
+
+  /**
+   * This method add item to checklist
+   */
+  addItemCheckList() {
+    commonActions.click(this.newChecklistItem);
+    commonActions.setValue(this.checklistNewItemText, 'description');
+    commonActions.click(this.checklistNewItemText);
+  }
+
+  /**
+   * This method delete check list
+   */
+  deleteCheckList() {
+    commonActions.click(this.confirmDelete);
+    commonActions.click(this.btnConfirmDeleteChecklist);
+  }
+}
+
+module.exports = card;
