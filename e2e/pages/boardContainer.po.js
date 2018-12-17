68f661f5ae0ef9939166c8662723b86e5990efb6
Rodrigo Menacho <rmenacho21@gmail.com>
2018-12-17 12:56:46 -0400

    Created the boardContainer PO

---
+++ e2e/pages/boardContainer.po.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const commonActions = require('../core/ui/commonActions');
+const dashboardForm = require('../');
+const dashboard = require('../pages/dashboard.po');
+
+class boardContainer {
+
+  /**
+   * Constructor for set the elements.
+   */
+  constructor() {
+    this.createNewBoardButton = '[class*="mod-add"][href="#"]';
+  }
+
+  /**
+   * This Method create a board in the board container PO.
+   */
+  createBoard() {
+    commonActions.click(this.createNewBoardButton);
+    return new dashboardForm();
+  }
+
+  /**
+   * Method for select a board in the board container PO.
+   *
+   * @param boardName Name of the board.
+   * @returns {Dashboard} The PO of the dashboard.
+   */
+  selectBoard(boardName) {
+    let board = `[class="board-tile-details-name"][title="${boardName}"]`;
+    commonActions.click(board);
+    return new dashboard();
+  }
+}
+
+module.exports = boardContainer;
