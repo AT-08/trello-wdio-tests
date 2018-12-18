6a1fe25d721942342b4069d7677ce16b6a12b061
Rodrigo Menacho <rmenacho21@gmail.com>
2018-12-18 16:49:33 -0400

    fixed lint

--- e2e/features/step_definitions/createBoard.step.js
+++ e2e/features/step_definitions/createBoard.step.js
@@ -1,16 +1,12 @@
 const {Given, When, Then} = require('cucumber');
-const expect = require('chai').expect;
 
 const config = require('../../core/ui/environment.config.json');
-
 const login = require('../../pages/login.po.js');
 
 let boardContainer;
 let dashboard;
 let dashboardForm;
 let loginPage;
-let titleString;
-let accessibilityString;
 const url = config.url;
 
 Given(/^I login with "([^"]*)"$/, (userKeys) => {
@@ -21,8 +17,6 @@ Given(/^I login with "([^"]*)"$/, (userKeys) => {
 When(/^I create a new Board with:$/, (dataTable) => {
   dashboardForm = boardContainer.createBoard();
   let rHash = dataTable.rowsHash();
-  titleString = rHash.Title;
-  accessibilityString = rHash.Privacy;
   dashboard = dashboardForm.createBoard(rHash);
 });
 
